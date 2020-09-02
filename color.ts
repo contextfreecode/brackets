f91f858f6d0c2c1403ba8c27d151af89d3cf45fb
Tom <tjpalmer@tjpalmer.com>
2020-09-02 05:58:16 -0700

    Simplify example

--- color.ts
+++ color.ts
@@ -1,78 +1,31 @@
-interface Color {
-  toRgb(): Rgb;
+type Rgb = { r: number; g: number; b: number };
+
+function rgbOf(r: number, g: number, b: number): Rgb {
+  return { r, g, b };
 }
 
-class Hsv implements Color {
-  constructor(h: number, s: number, v: number) {
-    this.h = h;
-    this.s = s;
-    this.v = v;
-  }
-
-  h: number;
-  s: number;
-  v: number;
-
-  toRgb() {
-    let { h, s, v } = this;
-    let c = s * v;
-    let h1 = h * 6;
-    let x = c * (1 - Math.abs(h1 % 2 - 1));
-    // deno-fmt-ignore
-    let [r1, g1, b1] =
-      h1 < 1 ? [c, x, 0] :
-      h1 < 2 ? [x, c, 0] :
-      h1 < 3 ? [0, c, x] :
-      h1 < 4 ? [0, x, c] :
-      h1 < 5 ? [x, 0, c] :
-      [c, 0, x];
-    let m = v - c;
-    return new Rgb(r1 + m, g1 + m, b1 + m);
-  }
+function darken({ r, g, b }: Rgb): Rgb {
+  let scale = 0.5;
+  return rgbOf(scale * r, scale * g, scale * b);
+  // TS 4.1 should allow for TupleN types.
+  // let scaled = [r, g, b].map((x) => scale * x) as [Item, Item, Item];
+  // return rgbOf.apply(null, scaled);
 }
 
-class NamedColor implements Color {
-  constructor(name: string) {
-    this.name = name;
-  }
-
-  name: string;
-
-  toRgb() {
-    return namedColors[this.name];
-  }
-}
-
-class Rgb implements Color {
-  constructor(r: number, g: number, b: number) {
-    this.r = r;
-    this.g = g;
-    this.b = b;
-  }
-
-  r: number;
-  g: number;
-  b: number;
-
-  toRgb() {
-    return this;
-  }
-}
-
+// Pretend I deep froze this or something.
 const namedColors: { [name: string]: Rgb } = {
-  red: new Rgb(1, 0, 0),
-  yellow: new Rgb(1, 1, 0),
-  blue: new Rgb(0, 0, 1),
+  red: rgbOf(1, 0, 0),
+  yellow: rgbOf(1, 1, 0),
+  blue: rgbOf(0, 0, 1),
 };
 
 function main() {
-  let colors: Color[] = [
-    new Hsv(1, 1, 1),
-    new NamedColor("yellow"),
-    new Rgb(0, 0, 1),
-  ];
-  console.log(colors);
-  console.log(colors.map((color) => color.toRgb()));
+  let names = ["red", "yellow", "blue"];
+  let rgbs = names.map((name) => namedColors[name]);
+  let darks = rgbs.map(darken);
+  for (let i = 0; i < names.length; i += 1) {
+    console.log(names[i], rgbs[i], darks[i]);
+  }
 }
 
 main();
