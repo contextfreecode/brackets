4ed05cdd103cd7463606265f3e33cf900b48d169
Tom <tjpalmer@tjpalmer.com>
2020-09-02 07:00:30 -0700

    Simplify clojure

---
+++ color.clj
@@ -0,0 +1,21 @@
+(ns color)
+
+(defrecord Rgb [r g b])
+
+(defn darken [{:keys [r g b]}]
+  (let [scale 0.5]
+    (apply ->Rgb (map #(* scale %) [r g b]))))
+
+(def ^:const named-colors
+  {:red (Rgb. 1.0 0.0 0.0)
+   :yellow (Rgb. 1.0 1.0 0.0)
+   :blue (Rgb. 0.0 0.0 1.0)})
+
+(defn main []
+  (let [names [:red :yellow :blue]
+        rgbs (into [] (map (partial get named-colors) names))
+        darks (into [] (map darken rgbs))]
+    (doseq [i (range (count names))]
+      (println (get names i) (get rgbs i) (get darks i)))))
+
+(main)
